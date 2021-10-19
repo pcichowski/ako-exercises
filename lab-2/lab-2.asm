5080057a8fe0f87cea7ebd44d903557f8bff9503
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-19 20:03:02 +0200

    Lab 2 Exercise 2.3

--- lab-2/lab-2.asm
+++ lab-2/lab-2.asm
@@ -6,31 +6,18 @@ extern _MessageBoxA@16 : PROC
 extern _MessageBoxW@16 : PROC
 public _main
 .data
-tytul_Unicode	dw 'T','e','k','s','t',' ','w',' '
-				dw 'f','o','r','m','a','c','i','e',' '
-				dw 'U','T','F','-','1','6', 0
-tekst_Unicode	dw 'K','a', 017Ch ,'d','y',' ','z','n','a','k',' '
-				dw 'z','a','j','m','u','j','e',' '
-				dw '1','6',' ','b','i','t', 00F3h ,'w', 0
-
-tytul_Win1250	db 'Tekst w standardzie Windows 1250', 0
-tekst_Win1250	db 'Ka', 0bfh, 'dy znak zajmuje 8 bit',  0f3h, 'w', 0
+tytul	dw	'Z', 'n', 'a', 'k', 'i', 0
+tekst	dw	'T', 'o', ' ', 'j', 'e', 's', 't', ' ', 'p','i','e','s', ' ',  0d83dh, 0dc08h, ' '
+		dw	'i', ' ', 'k', 'o', 't', ' ', 0d83dh, 0dc15h, 0
 
 .code
 _main PROC
 
-	push	0						; sta³a MB_OK
-	push	OFFSET tytul_Win1250	; adres obszaru zawieraj¹cego tytu³
-	push	OFFSET tekst_Win1250	; adres obszaru zawieraj¹cego tekst
-	push	0						; NULL (handle)
-	call	_MessageBoxA@16
-
-
-	push	0						; stala MB_OK
-	push	OFFSET tytul_Unicode	; adres obszaru zawieraj¹cego tytu³
-	push	OFFSET tekst_Unicode	; adres obszaru zawieraj¹cego tekst
-	push	0						; NULL (handle)
-	call	_MessageBoxW@16
+	push 0
+	push offset tytul
+	push offset tekst
+	push 0
+	call _MessageBoxW@16
 
 
 	push	0						; kod powrotu programu
