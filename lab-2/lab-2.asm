33e6df036c1eea55f4684cb10871f1b79f75d483
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2021-10-19 19:36:32 +0200

    Lab 2 Exercise 2.2

--- lab-2/lab-2.asm
+++ lab-2/lab-2.asm
@@ -1,28 +1,40 @@
-; program przyk³adowy (wersja 32-bitowa)
+; Przyk³ad wywo³ywania funkcji MessageBoxA i MessageBoxW
 .686
 .model flat
 extern _ExitProcess@4 : PROC
-extern __write : PROC		; (dwa znaki podkreœlenia)
+extern _MessageBoxA@16 : PROC
+extern _MessageBoxW@16 : PROC
 public _main
 .data
-	tekst 	db 10, 'Nazywam si', 0a9h, ' . . . ' , 10
-			db 'M', 0a2h ,'j pierwszy 32-bitowy program '
-			db 'asemblerowy dzia', 088h, 'a ju',0beh ,' poprawnie!', 10
+tytul_Unicode	dw 'T','e','k','s','t',' ','w',' '
+				dw 'f','o','r','m','a','c','i','e',' '
+				dw 'U','T','F','-','1','6', 0
+tekst_Unicode	dw 'K','a', 017Ch ,'d','y',' ','z','n','a','k',' '
+				dw 'z','a','j','m','u','j','e',' '
+				dw '1','6',' ','b','i','t', 00F3h ,'w', 0
+
+tytul_Win1250	db 'Tekst w standardzie Windows 1250', 0
+tekst_Win1250	db 'Ka', 0bfh, 'dy znak zajmuje 8 bit',  0f3h, 'w', 0
+
 .code
 _main PROC
-	mov 	ecx, 85 				; liczba znaków wyœwietlanego tekstu
 
-									; wywo³anie funkcji ”write” z biblioteki jêzyka C
-	push 	ecx 					; liczba znaków wyœwietlanego tekstu
-	push 	dword PTR OFFSET tekst 	; po³o¿enie obszaru
-									; ze znakami
-	push 	dword PTR 1 			; uchwyt urz¹dzenia wyjœciowego
-	call 	__write 				; wyœwietlenie znaków
-									; (dwa znaki podkreœlenia _ )
-	add 	esp, 12 				; usuniêcie parametrów ze stosu
+	push	0						; sta³a MB_OK
+	push	OFFSET tytul_Win1250	; adres obszaru zawieraj¹cego tytu³
+	push	OFFSET tekst_Win1250	; adres obszaru zawieraj¹cego tekst
+	push	0						; NULL (handle)
+	call	_MessageBoxA@16
 
-									; zakoñczenie wykonywania programu
-	push 	dword PTR 0 			; kod powrotu programu
-	call 	_ExitProcess@4
+
+	push	0						; stala MB_OK
+	push	OFFSET tytul_Unicode	; adres obszaru zawieraj¹cego tytu³
+	push	OFFSET tekst_Unicode	; adres obszaru zawieraj¹cego tekst
+	push	0						; NULL (handle)
+	call	_MessageBoxW@16
+
+
+	push	0						; kod powrotu programu
+	call	_ExitProcess@4
+
 _main ENDP
 END
